7a3e0488b6fe4a4ef838c4d185cbd8266376f6b8
Matthew Cook <cookm353@gmail.com>
2023-01-20 12:51:11 -0500

    tests for all get routes working

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -12,23 +12,25 @@ beforeEach(async () => {
         `INSERT INTO customers (first_name, last_name, phone, notes)
         VALUES ('Alice', 'Tester', '904-686-2332', 'test')
         RETURNING id, first_name, last_name, phone, notes;`
-        // `INSERT INTO customers (first_name, last_name, phone, notes)
-        // VALUES ('Alice', 'Tester', '904-686-2332', 'test')
-        // RETURNING id, first_name, last_name, phone, notes`
     )
     testCustomer = customerResult.rows[0]
 
+    const date = getDateString()
+
     const reservationResult = await db.query(
         `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
-        VALUES (${testCustomer.id}, 2023-02-08 12:20:07-07, 4, 'Double date')
-        RETURNING id, customer_id, start_at, num_guests, notes`
+        VALUES (${testCustomer.id}, '2023-02-14 18:30:00', 4, 'Double date')
+        RETURNING id, customer_id, start_at, num_guests, notes;`
+        // `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+        // VALUES (${testCustomer.id}, ${date}, 4, 'Double date')
+        // RETURNING id, customer_id, start_at, num_guests, notes;`
     )
     testReservation = reservationResult.rows[0]
 })
 
 afterEach(async () => {
-    await db.query(`DELETE FROM customers`)
     await db.query(`DELETE FROM reservations`)
+    await db.query(`DELETE FROM customers`)
 })
 
 afterAll(async () => {
@@ -37,7 +39,7 @@ afterAll(async () => {
 
 // Customer routes
 
-describe('/', () => {
+describe('GET /', () => {
     test("Getting landing page", async () => {
         const resp = await request(app).get('/')
         expect(resp.statusCode).toBe(200)
@@ -46,13 +48,15 @@ describe('/', () => {
     })
 })
 
-describe('/add', () => {
+describe('GET /add', () => {
     test("Add customer form", async () => {
         const resp = await request(app).get('/add')
         expect(resp.statusCode).toBe(200)
         expect(resp.text).toContain("<h1>Add a Customer</h1>")
     })
-    
+})
+
+describe('POST /add', () => {
     test('Adding new customer', async () => {
         const newCustomer = {
             firstName: "Bob",
@@ -65,6 +69,83 @@ describe('/add', () => {
             .post('/add')
             .send(newCustomer)
 
-        // expect(resp.statusCode).toBe(201)
+        // expect(resp.statusCode).toBe(302)
     })
 })
+
+describe('GET /search', () => {
+    test('Search for customer', async () => {
+        const resp = await request(app).get('/search?name=Alice')
+        
+        expect(resp.statusCode).toBe(200)
+        expect(resp.text).toContain("Alice")
+    })
+})
+
+describe('GET /best', () => {
+    test('Show customer if there are reservations', async () => {
+        const resp = await request(app).get('/best')
+        
+        expect(resp.statusCode).toBe(200)
+        expect(resp.text).toContain('Alice')
+    })
+
+    test("Don't list any customers if there aren't any reservations", async () => {
+        await db.query('DELETE FROM reservations')
+        const resp = await request(app).get('/best')
+        
+        expect(resp.statusCode).toBe(200)
+        expect(resp.text).not.toContain('Alice')
+    })
+})
+
+describe('GET /:id', () => {
+    test('Show customer detail page', async () => {
+        const resp = await request(app).get(`/${testCustomer.id}`)
+
+        expect(resp.statusCode).toBe(200)
+        expect(resp.text).toContain('Alice')
+        expect(resp.text).toContain('February')
+    })
+})
+
+describe('GET /:id/edit', () => {
+    test('Show customer edit form', async () => {
+        const resp = await request(app).get(`/${testCustomer.id}/edit`)
+        
+        expect(resp.text).toContain('Edit Customer')
+        expect(resp.text).toContain('Cancel')
+        expect(resp.text).toContain('test')
+    })
+})
+
+describe('POST /:id/add-reservation', () => {
+    test('Add reservation', async () => {
+        const resp = await request(app)
+            .post(`/${testCustomer.id}/add-reservation`)
+            .send({
+                startAt: "2023-2-14 19:00",
+                numGuests: 2
+            })
+
+        // expect(resp.statusCode).toBe(200)
+    })
+})
+
+function getDateString() {
+    const d = new Date()
+    let year = d.getFullYear()
+    let month = d.getMonth() + 1
+    let day = d.getDate()
+    let hour = d.getHours() 
+    let minute = d.getMinutes()
+    let seconds = d.getSeconds()
+    
+    if (month < 10) month = `0${month}`
+    if (day < 10) day = `0${day}`
+    if (hour < 10) hour = `0${hour}`
+    if (minute < 10) minute = `0${minute}`
+    if (seconds < 10) seconds = `0${seconds}`
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${seconds}`
+}
